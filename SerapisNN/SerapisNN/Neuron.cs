84cba614dd8092e3c5f7b673eba2213c4eb02ea5
dsrod <dsrod@seznam.cz>
2017-01-12 15:27:41 +0100

    Pokus o neuron LUL

---
+++ SerapisNN/SerapisNN/Neuron.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerapisNN
+{
+    /// <summary>
+    /// Sigmoid Neuron
+    /// </summary>
+    class Neuron
+    {
+        int bias;
+        Input[] inputs;
+
+        Random r = new Random();
+
+        Neuron(int bias)
+        {
+            this.bias = bias;
+        }
+
+        public void GenerateInputs(int n)
+        {
+            inputs = new Input[n];
+            
+
+            for(int i = 0; i < n; i++)
+            {
+                inputs[i] = new Input(r.Next(0,100));
+            }
+        }
+
+
+        public float Compute()
+        {
+            float w = 0;
+            foreach(Input i in inputs)
+            {
+                w = w + i.value * i.weight;
+            }
+            w = w - bias;
+
+            //SIGMA FUNCTION FOR THIS NEURON
+            float sig = 1 / (1 + (float)Math.Pow(Math.E, -w));
+
+            return sig;
+
+        }
+    }
+
+    public class Input
+    {
+
+        public int weight;
+        public float value = 0;
+
+        public Input(int w)
+        {
+            weight = w;
+        }
+
+        /// <summary>
+        /// Inputs value for this input
+        /// </summary>
+        /// <param name="v">Number from 0 to 1</param>
+        /// <returns></returns>
+        public bool setValue(float v)
+        {
+            if (v >= 0 && v <= 1)
+            {
+                value = v;
+                return true;
+            }
+            else return false;
+        }
+
+    }
+}
