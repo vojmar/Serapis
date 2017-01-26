d3ef1ec36dbdc2354b1f08242ab6fb1c0faa7b26
dsrod <dsrod@seznam.cz>
2017-01-26 16:12:10 +0100

    SOMETHIN SOMETHIN WE SHALL START ZE FEEDFORWARD!

--- SerapisNN/SerapisNN/Network.cs
+++ SerapisNN/SerapisNN/Network.cs
@@ -10,7 +10,8 @@ namespace SerapisNN
     {
 
         Layer[] layers;
-
+        int inputLayerNeuronsInputCount;
+        int inputLayerNeuronCount;
         /// <summary>
         /// Creates neural network with specified number of layers and neurons.
         /// Eq. creating 3 layer network with 2 neurons in first, 5 neurons in second and 1 neuron in third layers, do Network({2, 5, 1});
@@ -19,6 +20,11 @@ namespace SerapisNN
         /// <param name="inputLayerNeuronInputCount">Specifies number of inputs for input layer neurons</param>
         public Network(int[] layerNeuronCount, int inputLayerNeuronInputCount)
         {
+
+            this.inputLayerNeuronsInputCount = inputLayerNeuronInputCount;
+            this.inputLayerNeuronCount = layerNeuronCount[0];
+
+
             layers = new Layer[layerNeuronCount.Length];
             for(int n = 0; n < layerNeuronCount.Length; n++)
             {
@@ -27,11 +33,30 @@ namespace SerapisNN
                 {
                     layers[n].GenerateNeuronInputs(layerNeuronCount[n - 1]);
                 }
-                else //GENERATE 1 INPUT FOR EACH INPUT LAYER NEURON
+                else //GENERATE inputLayerNeuronInputCount INPUTS FOR EACH INPUT LAYER NEURON
                 {
                     layers[n].GenerateNeuronInputs(inputLayerNeuronInputCount);
                 }
             }
+        }
+
+        public float feedforward(Data[] d)
+        {
+            //DATA CHECK
+            if (d.Length != inputLayerNeuronCount) throw new Exception("Unexpected input data, neuron count does not correspond with input lenght");
+            foreach (Data dd in d)
+            {
+                if (dd.data.Length != inputLayerNeuronsInputCount) throw new Exception("Unexpected ammount of input data, neuron input count does not correspond with input data");
+            }
+            //DATA CHECK END
+
+            return 0; //nothing yet
+        }
+
+
+        public struct Data
+        {
+            public float[] data;
         }
     }
 }
